b18cb13df3d51befc7ffa9af36194ece8e492171
unknown <james@puffle.co.uk>
2015-05-29 21:00:08 +0100

    Add the post method

--- src/main/java/org/aurorawatchdevs/server/service/GCMNotifierService.java
+++ src/main/java/org/aurorawatchdevs/server/service/GCMNotifierService.java
@@ -7,6 +7,15 @@ import org.springframework.stereotype.Service;
 
 import com.google.appengine.api.datastore.Entity;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 @Service
 public class GCMNotifierService {
 
@@ -17,13 +26,12 @@ public class GCMNotifierService {
 
     @Value("${gcm.projectNumber}")
     private String projectNumber;
-    
+
     public void notifyGCM(Iterable<Entity> clients) {
         LOG.debug("notifyGCM: start");
 
         StringBuilder registrationIds = new StringBuilder();
         registrationIds.append("{ \"regstration_ids\": [");
-
         String splitter = "";
         for (Entity entity : clients) {
             LOG.info("notifying " + entity.getProperty("email"));
@@ -31,7 +39,45 @@ public class GCMNotifierService {
             splitter = ", ";
             registrationIds.append("\"" + entity.getProperty("registrationId") + "\"");
         }
-
         registrationIds.append("] }");
+
+        postNotifications(registrationIds.toString());
+
+    }
+
+    private void postNotifications(String message) {
+        try {
+            URL url = new URL("https://android.googleapis.com/gcm/send");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("POST");
+
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Authorization", "key=" + apiKey);
+            conn.setDoOutput(true);
+
+            OutputStreamWriter streamWriter = new OutputStreamWriter(conn.getOutputStream());
+            streamWriter.write(message);
+            streamWriter.flush();
+            streamWriter.close();
+
+            int responseCode = conn.getResponseCode();
+            System.out.println("\nSending 'POST' request to URL : " + url);
+            System.out.println("Response Code : " + responseCode);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
